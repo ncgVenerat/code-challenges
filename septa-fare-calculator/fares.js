6d7c58ed726d87a013c004af09bee9bedd164ae1
ncgVenerat <nviiicg@gmail.com>
2016-07-28 12:51:47 -0400

    Create fares.js
    Setting up variables, trying to make a working version with JS.

---
+++ septa-fare-calculator/fares.js
@@ -0,0 +1,33 @@
+function getFare () {
+var zone = ;
+var time = ;
+var where = ;
+var ticket = ;
+
+var fareZone = ;
+var fareTime = ;
+var fareWhere = ;
+var fareTicket = ;
+
+if (zone = 1) {
+  var fareZone = 4.75;
+  var fareTime = -1.00;
+  var fareWhere = 1.25;
+} else if (zone = 2) {
+  var fareZone = 4.75;
+  var fareTime = -1.00;
+  var fareWhere = 1.25;
+} else if (zone = 3){
+  var fareZone = 5.75;
+  var fareTime = -0.75;
+  var fareWhere = 1.25;
+} else if (zone = 4){
+  var fareZone = 6.50;
+  var fareTime = -1.50;
+  var fareWhere = 1.50;
+} else if (zone = 5){
+  var fareZone = 9.00;
+  var fareTime = 0;
+  var fareWhere = 1.00;
+}
+}
